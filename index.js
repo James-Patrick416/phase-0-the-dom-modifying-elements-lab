722b797442cae26f8bcf25d4cab66cbac9c97814
Your Name <you@example.com>
2025-06-29 03:40:16 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code here!
+// Remove the main element from the DOM
+document.querySelector("main").remove();
+
+// Create a new h1 element
+const newHeader = document.createElement("h1");
+
+// Set its id to 'victory'
+newHeader.id = "victory";
+
+// Set its text content
+newHeader.textContent = "James Patrick is the champion";
+
+// Append it to the body
+document.body.append(newHeader);
